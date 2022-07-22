aa6ac548f181e63199f1843631892bb28a2b28b0
Daniel Madureira <danielmadureira@MacBook-Pro-de-Daniel.local>
2022-07-22 00:20:49 -0300

    Nome database

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-exports.initDb = (connectionURL) => {
+exports.initDb = (connectionURL, database="authentication") => {
     mongoose.connect(connectionURL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         // useCreateIndex: true,
-        dbName: "authentication",
+        dbName: database,
     }).then(success => console.log("Db authentication conected!"))
 }
