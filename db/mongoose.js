37fa5672b8ce68d4d48611b7f66fe6ddc970b8d7
Daniel <madureiradaniel600@gmail.com.br>
2022-12-30 15:30:32 -0300

    FIX - strictQuery

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+mongoose.set('strictQuery', true)
+
 exports.initDb = (connectionURL, database="authentication") => {
     mongoose.connect(connectionURL, {
         useNewUrlParser: true,
